55837ed639fd7376f630e12b714e41982058ede3
KurtDev599 <sw536880@naver.com>
2022-11-06 18:17:19 +0900

    mongoose init setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://swkim:1234@nestcluster.oynfqka.mongodb.net/test',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
